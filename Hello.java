dc036d3925b6d97bb5b647b554376f3fefddc0cf
Jordan S-H <jstebner@umich.edu>
2022-09-27 16:46:29 -0400

    added new method

--- Hello.java
+++ Hello.java
@@ -2,4 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World")    
     }
+
+    public void method1() {
+        System.out.pritnln("this a new method :)")
+    }
 }
