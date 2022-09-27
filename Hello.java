da4839fd03d3aaedced80364a2711638014dae00
Jordan S-H <jstebner@umich.edu>
2022-09-27 17:03:29 -0400

    added semi colons :|

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World")    
+        System.out.println("Hello World");
     }
 
     public void method1() {
-        System.out.println("this a new method :)")
+        System.out.println("this a new method :)");
     }
 }
